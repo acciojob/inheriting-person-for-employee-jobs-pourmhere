7570bc2588f3747086f2c7f4e2c261a3212287ac
POURMHERE <103439512+pourmhere@users.noreply.github.com>
2024-08-19 09:18:02 -0700

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,20 @@
-// complete this js code
-function Person(name, age) {}
+function Person(name, age) {
+	this.name:name;
+		this.age:age;
 
-function Employee(name, age, jobTitle) {}
+	this.greet()=function(){
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`)
+	}
 
+function Employee(name, age, jobTitle) {
+	this.jobTitle:jobTitle;
+
+	this.jobGreet()=function(){
+		console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}`)
+	};
+}
+Employee.prototype = Object.create(Person.prototype);
+Employee.prototype.constructor = Employee;
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
